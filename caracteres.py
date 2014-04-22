87212ae2d9e79d50ac0fce90c8ef289f3ed6b167
FelipeBala <felipebalabanian@gmail.com>
2014-04-21 23:36:20 -0300

    Signed-off-by: FelipeBala <felipebalabanian@gmail.com>

---
+++ caracteres.py
@@ -0,0 +1,346 @@
+import pygame
+import spritesheet_functions
+import time
+import threading
+
+class grafica():
+
+	lock = threading.Lock()
+	
+	def __init__(self,screen2,L,background_image2,imagem_trem2):
+		self.screen4 = screen2
+		self.imagem2 = []
+		self.pos = []
+		self.u = []
+		self.L1 = L
+		self.background_image = background_image2
+		self.imagem_trem = imagem_trem2
+		self.pos_trem = [-900,410]
+		
+	def imagem(self,ima,pos2,x):
+		grafica.lock.acquire()
+		flag = -1
+		for i in range(len(self.u)):
+			if self.u[i] == x :
+				flag = 2
+				self.imagem2[i] = ima
+				self.pos[i] = pos2
+				break
+		if flag == -1:
+			self.imagem2.append(ima)
+			self.pos.append(pos2)
+			self.u.append(x)
+		grafica.lock.release()
+			
+	def deleta(self,x):
+		grafica.lock.acquire()
+		for i in range(len(self.u)):
+			if self.u[i] == x :
+				del self.imagem2[i]
+				del self.pos[i]
+				del self.u[i]
+				break
+		grafica.lock.release()
+
+				
+	def train_chega(self):
+		if self.pos_trem[0] != -900:
+			self.pos_trem[0] -= 2000
+		for j in range(len(self.u)):
+			if self.pos[j][1] > 400 and self.pos[j][1] < 570:
+				self.pos[j][0] -= 2000
+			
+		for i in range(self.pos_trem[0],0,2):
+			for j in range(len(self.u)):
+				if self.pos[j][1] > 400 and self.pos[j][1] < 570:
+					self.pos[j][0] += 2
+			self.pos_trem[0] = i
+			time.sleep(0.01)
+	
+	def train_parte(self):
+		for i in range(self.pos_trem[0],1100,2):
+			for j in range(len(self.u)):
+				if self.pos[j][1] > 400 and self.pos[j][1] < 570:
+					self.pos[j][0] += 2
+			self.pos_trem[0] = i
+			time.sleep(0.01)
+
+	def imprime(self):
+		grafica.lock.acquire()
+		#self.screen4.fill([ 0, 0, 0])
+		self.screen4.blit(self.background_image, [0, 0])
+		self.screen4.blit(self.imagem_trem, self.pos_trem)
+		for t in range(len(self.u)):
+			#print("quero saber= "+repr(self.u[t]))
+			#print("quero pos_0= "+repr(self.pos[t][0]))
+			#print("quero pos_1= "+repr(self.pos[t][1]))
+			self.screen4.blit( self.imagem2[t], self.pos[t])
+			pygame.event.get()
+		pygame.event.get()
+		pygame.display.flip()
+		grafica.lock.release()
+		
+
+class inicia_personagens():
+    imagem = {}
+    num_personagens = 3
+    def __init__(self):
+        k=0
+        teste = spritesheet_functions.SpriteSheet("sheet.png")
+        for i in range(0,8,1):
+            for j in range(0,12,1):
+                self.imagem[k] = teste.get_image(j*32,i*32,32,32)
+                k+= 1
+                
+                
+class personagem():
+    x = 0
+    y = 0
+    estado = 0
+    screen = None
+    id = 0
+    velocidade = 0.001
+    
+    def __init__(self,imagem,num,screen20,grafico2,num2):
+		
+        self.screen = screen20
+        self.grafico = grafico2
+        self.id = num2
+        self.personagem = {}
+		
+        if num == 1 :
+            self.personagem[0] = imagem[0]
+            self.personagem[1] = imagem[1]
+            self.personagem[2] = imagem[2]
+            
+            self.personagem[3] = imagem[12]
+            self.personagem[4] = imagem[13]
+            self.personagem[5] = imagem[14]
+            
+            self.personagem[6] = imagem[24]
+            self.personagem[7] = imagem[25]
+            self.personagem[8] = imagem[26]
+		
+        if num == 2 :
+            self.personagem[0] = imagem[3]
+            self.personagem[1] = imagem[4]
+            self.personagem[2] = imagem[5]
+            
+            self.personagem[3] = imagem[15]
+            self.personagem[4] = imagem[16]
+            self.personagem[5] = imagem[17]
+            
+            self.personagem[6] = imagem[27]
+            self.personagem[7] = imagem[28]
+            self.personagem[8] = imagem[29]
+
+        if num == 3 :
+            self.personagem[0] = imagem[6]
+            self.personagem[1] = imagem[7]
+            self.personagem[2] = imagem[8]
+            
+            self.personagem[3] = imagem[18]
+            self.personagem[4] = imagem[19]
+            self.personagem[5] = imagem[20]
+            
+            self.personagem[6] = imagem[30]
+            self.personagem[7] = imagem[31]
+            self.personagem[8] = imagem[32]
+
+        if num == 4 :
+            self.personagem[0] = imagem[9]
+            self.personagem[1] = imagem[10]
+            self.personagem[2] = imagem[11]
+            
+            self.personagem[3] = imagem[21]
+            self.personagem[4] = imagem[22]
+            self.personagem[5] = imagem[23]
+            
+            self.personagem[6] = imagem[33]
+            self.personagem[7] = imagem[34]
+            self.personagem[8] = imagem[35]
+		
+        if num == 5 :
+            self.personagem[0] = imagem[48]
+            self.personagem[1] = imagem[49]
+            self.personagem[2] = imagem[50]
+            
+            self.personagem[3] = imagem[60]
+            self.personagem[4] = imagem[61]
+            self.personagem[5] = imagem[62]
+            
+            self.personagem[6] = imagem[72]
+            self.personagem[7] = imagem[73]
+            self.personagem[8] = imagem[74]
+
+        if num == 6 :
+            self.personagem[0] = imagem[51]
+            self.personagem[1] = imagem[52]
+            self.personagem[2] = imagem[53]
+            
+            self.personagem[3] = imagem[63]
+            self.personagem[4] = imagem[64]
+            self.personagem[5] = imagem[65]
+            
+            self.personagem[6] = imagem[75]
+            self.personagem[7] = imagem[76]
+            self.personagem[8] = imagem[77]
+
+        if num == 7 :
+            self.personagem[0] = imagem[54]
+            self.personagem[1] = imagem[55]
+            self.personagem[2] = imagem[56]
+            
+            self.personagem[3] = imagem[66]
+            self.personagem[4] = imagem[67]
+            self.personagem[5] = imagem[68]
+            
+            self.personagem[6] = imagem[78]
+            self.personagem[7] = imagem[79]
+            self.personagem[8] = imagem[80]
+
+        if num == 8 :
+            self.personagem[0] = imagem[57]
+            self.personagem[1] = imagem[58]
+            self.personagem[2] = imagem[59]
+            
+            self.personagem[3] = imagem[69]
+            self.personagem[4] = imagem[70]
+            self.personagem[5] = imagem[71]
+            
+            self.personagem[6] = imagem[81]
+            self.personagem[7] = imagem[82]
+            self.personagem[8] = imagem[83]			
+		
+    @staticmethod
+    def inicia_parametros(v):
+        personagem.velocidade = v
+        
+    
+    def desenha(self):
+        self.grafico.imagem(self.personagem[self.estado],[self.x,self.y],self.id)
+
+    
+    def pos_init(self,x1,y1):
+        self.x = x1
+        self.y = y1
+    
+    def move_R(self):
+        self.x += 2
+        if self.estado == 6:
+            self.estado = 7
+        elif self.estado == 7:
+            self.estado = 8 
+        elif self.estado == 8:
+            self.estado = 6
+        else:
+            self.estado = 7
+        self.desenha()
+
+    def move_L(self):
+        self.x -= 2
+        if self.estado == 5:
+            self.estado = 4
+        elif self.estado == 4:
+            self.estado = 3 
+        elif self.estado == 3:
+            self.estado = 5
+        else:
+            self.estado = 4
+        self.desenha()        
+        
+    def move_D(self):
+        self.y += 2
+        if self.estado == 0:
+            self.estado = 1
+        elif self.estado == 1:
+            self.estado = 2 
+        elif self.estado == 2:
+            self.estado = 0
+        else:
+            self.estado = 1
+        self.desenha()        
+    
+    def chegue_em(self,targ_x,targ_y):
+        
+        for i in range(self.y,targ_y,2):
+            self.move_D()
+            time.sleep(personagem.velocidade)			
+            
+        if (targ_x-self.x)>0:
+            for i in range(self.x,targ_x,2):
+                self.move_R()
+                pygame.event.get()
+                time.sleep(personagem.velocidade)
+
+        else:
+            for i in range(self.x,targ_x,-2):
+                self.move_L()
+                pygame.event.get()
+                time.sleep(personagem.velocidade)
+            
+
+class rollercoaster():
+	max_pessoas_fila = 6
+	max_pessoas_trem = 4
+	fila_pos = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+	fila_pos_u = [[750,330],[710,330],[670,330],[630,330],[550,330],[510,330],[470,330],[430,330],[350,330],[310,330],[270,330],[240,330]]
+	fila_trem = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+	fila_trem_u = [[750,430],[710,430],[670,430],[630,430],[550,430],[510,430],[470,430],[430,430],[350,430],[310,430],[270,380],[240,380]]
+	def __init__(self,personagem2):
+		self.personagem = personagem2
+	
+	@staticmethod
+	def inicia_parametros(max_fila,max_trem):
+		if max_fila > 30:
+			print("O numero maximo de pessoas na fila precisa ser menor que 30")
+		else:
+			rollercoaster.max_pessoas_fila = max_fila 
+		if max_trem > 30:
+			print("O numero maximo de pessoas no trem precisa ser menor que 30")
+		else:
+			rollercoaster.max_pessoas_trem = max_trem  
+
+	def vai_para_fila(self):
+		if self.flag1 == -1:
+			self.personagem.chegue_em(110,170)
+			self.personagem.chegue_em(-40,170)
+			return
+		self.personagem.chegue_em(rollercoaster.fila_pos_u[self.flag1][0],rollercoaster.fila_pos_u[self.flag1][1])
+		return
+
+	def pega_posicao(self):
+		self.flag1=-1
+		for i in range(0,rollercoaster.max_pessoas_fila,1):
+			if rollercoaster.fila_pos[i] == 0:
+				self.flag1 = i
+				rollercoaster.fila_pos[i] = 1
+				break
+		if self.flag1 == -1:
+			return(-1)
+		return(2)
+
+	
+	def vai_para_o_trem(self):
+		for i in range(0,rollercoaster.max_pessoas_trem,1):
+			if rollercoaster.fila_trem[i] == 0:
+				flag2 = i
+				rollercoaster.fila_trem[i] = 1
+				break
+		self.personagem.chegue_em(rollercoaster.fila_trem_u[flag2][0],rollercoaster.fila_trem_u[flag2][1])
+	
+	@staticmethod
+	def limpa_vagao():
+		for i in range(0,29,1):
+			rollercoaster.fila_trem[i] = 0
+	
+	
+	def sai_do_trem(self):
+		self.personagem.chegue_em(420,650)
+		self.personagem.chegue_em(420,800)
+
+	def remove(self):
+		self.personagem.grafico.deleta(self.personagem.id)
+	
+	def limpa_vaga(self):
+		rollercoaster.fila_pos[self.flag1] = 0
---
+++ rollercoaster2.py
@@ -0,0 +1,156 @@
+﻿import threading
+import pygame
+import caracteres
+import time
+
+entra_no_trem = threading.Lock()
+sai_do_trem = threading.Lock()
+ha_trem = threading.Condition()
+chegou_trem = threading.Condition()
+fila = threading.Lock()
+fila2 = threading.Lock()
+fila3 = threading.Lock()
+LL = threading.Condition()
+
+pessoas_sentadas = 0
+pessoas_fila = 0
+
+#---------Input de parametros ---------
+a = int(input("Max pessoas na Fila"))
+b = int(input("Max pessoas no Trem"))
+c = float(input("Tempo de passeo do Trem"))
+d = float(input("Tempo de Spawn das Pessoas"))
+e = float(input("Velocidade das pessoas (ms)"))
+
+e = e/1000
+caracteres.rollercoaster.inicia_parametros(a,b)
+caracteres.personagem.inicia_parametros(e)
+lugares = threading.Semaphore(b)
+#------------------------------------------
+
+pygame.init()
+screen = pygame.display.set_mode([1038, 718])
+#screen = pygame.display.set_mode([800, 600])
+background_image = pygame.image.load("background.jpg").convert()
+imagem_trem = pygame.image.load("trem.jpg").convert()
+grafico = caracteres.grafica(screen,LL,background_image,imagem_trem)
+pygame.display.set_caption('Teste')
+clock = pygame.time.Clock()
+base = caracteres.inicia_personagens() 
+
+def pessoa(num):
+	global grafico
+	global screen
+	global base
+	global pessoas_fila
+	global pessoas_sentadas
+	
+	#Inicia
+	u = num % 8 +1
+	personagem1 = caracteres.personagem(base.imagem,u,screen,grafico,num)
+	personagem1.pos_init(120,0)
+	p1 = caracteres.rollercoaster(personagem1)
+	
+	#vai para a fila
+	fila.acquire()
+	if p1.pega_posicao() == -1:
+		p1.vai_para_fila()
+		fila.release()
+		p1.remove()
+		print(repr(num)+"=Fila cheia, vou embora")
+		return
+	fila.release()	
+	p1.vai_para_fila()
+	
+	print(repr(num)+"= já estou na fila")
+	
+	#Pega ticket // Ve se o trem chegou
+	lugares.acquire()
+	print(repr(num)+"= já peguei o ticket do trem")
+	ha_trem.acquire()
+	ha_trem.wait()
+	ha_trem.release()
+	
+	# Vai se sentar
+	fila2.acquire()
+	pessoas_fila+=1
+	p1.limpa_vaga()
+	p1.vai_para_o_trem()
+	print(repr(num)+"= já estou sentado no trem")
+	fila2.release()
+	
+	#todos sentados
+	chegou_trem.acquire()
+	pessoas_sentadas+=1
+	chegou_trem.wait()
+	#Trem partiu
+	#Trem chegou
+	chegou_trem.release()
+	p1.sai_do_trem()
+	#vao embora
+	fila3.acquire()
+	p1.remove()
+	fila3.release()
+	lugares.release()
+	
+	
+	print(repr(num)+"= já fui embora")
+	
+	
+def trem(num):
+	global pessoas_fila
+	global pessoas_sentadas
+	global grafico
+	global a
+	global b
+	global c
+	grafico.train_chega()
+	while True:
+		
+		#aqui
+		
+		while pessoas_fila != b:
+			ha_trem.acquire()
+			ha_trem.notify_all()
+			ha_trem.release()
+			time.sleep(0.1)
+			pass
+		pessoas_fila=0
+		print("Passageiros carregados")
+		while pessoas_sentadas != b:
+			pass
+		chegou_trem.acquire()
+		chegou_trem.notify_all()
+		grafico.train_parte()
+		time.sleep(c)
+		grafico.train_chega()
+		chegou_trem.release()
+		#sleep
+		print("Passageiros descarregados")
+		caracteres.rollercoaster.limpa_vagao()
+		pessoas_sentadas = 0
+	
+
+def timer_grafico():
+
+	while True:
+		grafico.L1.acquire()
+		grafico.imprime()
+		time.sleep(0.01)
+		grafico.L1.release()
+		
+		
+		
+timer = threading.Thread(target=timer_grafico)
+timer.start()	
+Trem = threading.Thread(target=trem,args=[999])
+Trem.start()
+
+for i in range(500):
+	T = threading.Thread(target=pessoa,args=[i])
+	T.start()
+	time.sleep(d)
+	
+
+timer.join()
+pygame.quit ()
